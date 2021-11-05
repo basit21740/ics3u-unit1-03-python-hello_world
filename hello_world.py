f2da5a00d701f39f1401eebe05b2255e4d550131
abdul <abdul.butt@mths.ca>
2021-11-05 17:26:52 +0000

    added comments and placed bye in functions

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Abdul Basit
+# Created on: Nov 2021
+# This is the "Hello,World!" program, with proper style
+
+
+def main():
+    print("Hello,World!")
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
